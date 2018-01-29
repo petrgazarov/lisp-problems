b2bd46ce7f3adcb4f39e4c8e53c6e43ff2835938
Petr Gazarov <petrgazarov@gmail.com>
2018-01-29 14:41:12 -0800

    Add sum function with iterative approach

---
+++ sum_function_iterative.scm
@@ -0,0 +1,15 @@
+;;; Sum function using iterative approach.
+
+(define (sum term a next b)
+  (define (iter a result)
+      (if (> a b)
+          result
+          (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (cube x) (* x x x))
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(display (sum-cubes 1 10))
